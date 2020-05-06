e37dc1a666a01c2206dc7902cd352a7beaa3e138
AlexPawelczyk <alex.pawelczyk@uwaterloo.ca>
2020-05-05 22:21:11 -0400

    Added ball to game canvas

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import Paddle from "/src/paddle.js"
 import InputHandler from "/src/InputHandler.js";
+import Ball from "/src/ball.js";
 
 var canvas = document.getElementById("gameScreen");
 var ctx = canvas.getContext("2d"); // rendering context for drawing to canvas
 
 const GAME_HEIGHT = 600;
 const GAME_WIDTH = 800;
-ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
 
 // instantiate Paddle object
 var paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-paddle.draw(ctx);
 
-var inputHandler = new InputHandler(paddle);
+// instantiate Ball object
+var ball = new Ball();
 
+// instantiate InputHandler object for moving the paddle
+new InputHandler(paddle);
+
+// initialize last time to 0
 var lastTime = 0;
 
-// gamelopp runs every frame
+// gameloop runs every frame
 function gameLoop(timestamp) {
   // calculate how much time has passed
   var deltaTime = timestamp - lastTime;
@@ -30,6 +35,9 @@ function gameLoop(timestamp) {
 
   // redraws paddle
   paddle.draw(ctx);
+
+  // redraw ball
+  ball.draw(ctx);
 
   // calls the game loop again with next frames time stamp
   requestAnimationFrame(gameLoop);
