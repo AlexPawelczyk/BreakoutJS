c8d1294cd6e9fb61bf7eec4e59605c8d1a7206c6
AlexPawelczyk <alex.pawelczyk@uwaterloo.ca>
2020-05-06 20:48:11 -0400

    added collision detection between ball and paddle

--- src/ball.js
+++ src/ball.js
@@ -1,5 +1,6 @@
 export default class Ball {
   constructor(game){
+    this.game = game;
     this.gameHeight = game.gameHeight;
     this.gameWidth = game.gameWidth;
     this.image = document.getElementById("img_ball");
@@ -21,11 +22,28 @@ export default class Ball {
   update(deltaTime) {
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
+
+    // collision with left or right walls
     if(this.position.x + this.size > this.gameWidth || this.position.x < 0) {
       this.speed.x = -this.speed.x;
     }
+
+    // collision with top or bottom walls
     if(this.position.y + this.size > this.gameHeight || this.position.y < 0) {
       this.speed.y = -this.speed.y;
     }
+
+    // collision with paddle
+    var ballBottom = this.position.y + this.size;
+    var paddleTop = this.game.paddle.position.y;
+    var paddleLeft = this.game.paddle.position.x;
+    var paddleRight = this.game.paddle.position.x + this.game.paddle.paddleWidth;
+
+    if(ballBottom >= paddleTop && this.position.x >= paddleLeft && this.position.x <= paddleRight) {
+      this.speed.y = -this.speed.y;
+      this.position.y = this.game.paddle.position.y - this.size;
+    }
+
+
   }
 }
